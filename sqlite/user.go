d544eb605c01303fbb20bf85354b6856f281931e
Eric Ekholm <eric.ekholm@gmail.com>
2026-03-23 15:52:12 -0400

    add sqlite userservice methods

--- sqlite/user.go
+++ sqlite/user.go
@@ -40,17 +40,100 @@ func (us userService) CreateUser(ctx context.Context, username string) (int, err
 }
 
 func (us userService) GetUserByUsername(ctx context.Context, username string) (*flexcreek.User, error) {
-	return nil, nil
+	qry := `
+		SELECT id,
+		username,
+		created_at
+		FROM users
+		WHERE username = ?	
+	`
+
+	var u flexcreek.User
+
+	res := us.db.QueryRowContext(ctx, qry, username)
+
+	//todo later -- define a custom error type to handle cases where no rows are returned & return this instead
+	if err := res.Scan(&u.ID, &u.Username, &u.CreatedAt); err != nil {
+		return nil, err
+	}
+
+	return &u, nil
 }
 
 func (us userService) GetUserByID(ctx context.Context, id int) (*flexcreek.User, error) {
-	return nil, nil
+	qry := `
+		SELECT id,
+		username,
+		created_at
+		FROM users
+		WHERE id = ?	
+	`
+
+	var u flexcreek.User
+
+	res := us.db.QueryRowContext(ctx, qry, id)
+
+	//todo later -- define a custom error type to handle cases where no rows are returned & return this instead
+	if err := res.Scan(&u.ID, &u.Username, &u.CreatedAt); err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+
 }
 
 func (us userService) GetAllUsernames(ctx context.Context) ([]string, error) {
-	return nil, nil
+	qry := `
+		SELECT username
+		FROM users	
+	`
+
+	rows, err := us.db.QueryContext(ctx, qry)
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var usernames []string
+	for rows.Next() {
+		var username string
+		if err := rows.Scan(&username); err != nil {
+			return nil, err
+		}
+		usernames = append(usernames, username)
+	}
+
+	//check for any errors that occur during iteration
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return usernames, nil
 }
 
 func (us userService) DeleteUser(ctx context.Context, id int) error {
+	qry := `
+		DELETE FROM users
+		WHERE id = ?	
+	`
+
+	res, err := us.db.ExecContext(ctx, qry, id)
+	if err != nil {
+		return err
+	}
+
+	// Check if any rows were actually deleted.
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	// If no rows were affected, it means the user with that ID was not found.
+	if rowsAffected == 0 {
+		// In the future, this could return a custom flexcreek.ErrNotFound.
+		return sql.ErrNoRows
+	}
+
 	return nil
 }
