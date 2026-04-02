51ca76614cf6ec4e7e5ef953cdf3aba855535c12
Eric Ekholm <eric.ekholm@gmail.com>
2026-04-02 13:32:10 -0400

    finish workoutService db methods

--- sqlite/workout.go
+++ sqlite/workout.go
@@ -3,6 +3,7 @@ package sqlite
 import (
 	"context"
 	"database/sql"
+	"errors"
 	"time"
 
 	"github.com/ekholme/flexcreek"
@@ -73,7 +74,7 @@ func (ws workoutService) GetWorkoutByDate(ctx context.Context, date time.Time, u
 		workout_date,
 		created_at
 		FROM workouts
-		WHERE id = ?
+		WHERE user_id = ?
 		  AND date = ?
 	`
 
@@ -89,13 +90,97 @@ func (ws workoutService) GetWorkoutByDate(ctx context.Context, date time.Time, u
 }
 
 func (ws workoutService) GetLatestWorkouts(ctx context.Context, n int, userID int) ([]*flexcreek.Workout, error) {
-	return nil, nil
+	qry := `
+		SELECT id,
+		user_id,
+		short_description,
+		long_description,
+		workout_date,
+		created_at
+		FROM workouts
+		WHERE user_id = ?	
+		ORDER BY workout_date desc
+		LIMIT ?;
+	`
+
+	rows, err := ws.db.QueryContext(ctx, qry, userID, n)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var workouts []*flexcreek.Workout
+
+	for rows.Next() {
+		var w flexcreek.Workout
+
+		err = rows.Scan(&w.ID, &w.UserID, &w.ShortDescription, &w.LongDescription, &w.WorkoutDate, &w.CreatedAt)
+
+		if err != nil {
+			return nil, err
+		}
+
+		workouts = append(workouts, &w)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return workouts, nil
 }
 
 func (ws workoutService) UpdateWorkout(ctx context.Context, w *flexcreek.Workout) error {
+	qry := `
+		UPDATE workouts
+		SET short_description = ?,
+		long_description = ?,
+		workout_date = ?
+		WHERE id = ?
+		  AND user_id = ?
+	`
+
+	res, err := ws.db.ExecContext(ctx, qry, w.ShortDescription, w.LongDescription, w.WorkoutDate, w.ID, w.UserID)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		return nil
+	}
+
+	if rowsAffected == 0 {
+		return errors.New("no workout found with that ID for this user")
+	}
+
 	return nil
 }
 
 func (ws workoutService) DeleteWorkout(ctx context.Context, id int) error {
+	qry := `
+		DELETE FROM workouts WHERE id = ?	
+	`
+
+	res, err := ws.db.ExecContext(ctx, qry, id)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := res.RowsAffected()
+
+	if err != nil {
+		return err
+	}
+
+	if rowsAffected == 0 {
+		return sql.ErrNoRows
+	}
+
 	return nil
 }
