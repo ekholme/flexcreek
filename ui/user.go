377fb022520b5d68af16d0e7af279067398696b1
Eric Ekholm <eric.ekholm@gmail.com>
2026-04-15 15:09:35 -0400

    work on usermodel update and view methods

--- ui/user.go
+++ ui/user.go
@@ -4,8 +4,14 @@ import (
 	"context"
 
 	"github.com/charmbracelet/bubbles/list"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/ekholme/flexcreek"
+)
+
+const (
+	stateList sessionState = iota
+	stateCreateUser
 )
 
 // defining interfaces that the user model currently requires
@@ -31,6 +37,8 @@ type UserStore interface {
 type UserModel struct {
 	store    UserStore
 	list     list.Model
+	input    textinput.Model
+	state    sessionState
 	loading  bool
 	err      error
 	selected *flexcreek.User
@@ -41,9 +49,16 @@ func NewUserModel(s UserStore) UserModel {
 	l := list.New([]list.Item{}, list.NewDefaultDelegate(), 0, 0)
 	l.Title = "Select a User"
 
+	//text input stuff
+	ti := textinput.New()
+	ti.Placeholder = "New Username..."
+	ti.Focus()
+
 	return UserModel{
 		store:   s,
 		list:    l,
+		input:   ti,
+		state:   stateList,
 		loading: true,
 	}
 }
@@ -66,6 +81,10 @@ type usersLoadedMsg struct {
 	users []*flexcreek.User
 }
 
+type userSelectedMsg struct {
+	user *flexcreek.User
+}
+
 type userItem struct {
 	flexcreek.User
 }
@@ -77,13 +96,18 @@ func (i userItem) FilterValue() string { return i.Username }
 //bubbletea requires models to implement 3 methods -- Init(), Update(), and View()
 
 func (m UserModel) Init() tea.Cmd {
-	//todo
+	return fetchUsersCmd(m.store)
 }
 
 func (m UserModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
 
 	switch msg := msg.(type) {
+	case error:
+		m.loading = false
+		m.err = msg
+		return m, nil
+
 	case usersLoadedMsg:
 		m.loading = false
 		items := make([]list.Item, len(msg.users))
@@ -92,22 +116,64 @@ func (m UserModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		}
 		m.list.SetItems(items)
 
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "enter":
-			if i, ok := m.list.SelectedItem().(userItem); ok {
-				m.selected = &i.User
-				return m, func() tea.Msg { return UserSelectedMsg{i.User} }
-			}
-		case "n":
-			// Logic to switch to a "Create User" form state
+	case tea.WindowSizeMsg:
+		h, v := msg.Width, msg.Height
+		m.list.SetSize(h, v)
+
+		switch m.state {
+		case stateList:
+			return m.updateList(msg)
+		case stateCreateUser:
+			return m.updateForm(msg) //TODO
 		}
 	}
-
-	m.list, cmd = m.list.Update(msg)
 	return m, cmd
 }
 
 func (m UserModel) View() string {
-	//todo
+	if m.err != nil {
+		return "Error: " + m.err.Error()
+	}
+
+	switch m.state {
+	case stateCreateUser:
+		return "\n Create New User \n\n" +
+			m.input.View() +
+			"\n\n (esc to go back)"
+
+	default:
+		if m.loading {
+			return " Loading users..."
+		}
+
+		return "\n" + m.list.View()
+	}
+
+}
+
+// helper update functions for different views
+func (m UserModel) updateList(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		//prevents user selection if we're in a filtering state
+		if m.list.FilterState() == list.Filtering {
+			break
+		}
+		switch msg.String() {
+		case "n":
+			m.state = stateCreateUser
+			m.input.Focus()
+			return m, nil
+
+		case "enter":
+			if i, ok := m.list.SelectedItem().(userItem); ok {
+				m.selected = &i.User
+				return m, func() tea.Msg { return userSelectedMsg{&i.User} }
+			}
+		}
+
+	}
+	var cmd tea.Cmd
+	m.list, cmd = m.list.Update(msg)
+	return m, cmd
 }
